81e2980d013faf26ed53748a553cb80f701eb1a2
Domyorke <dominicjenkins@hotmail.com>
2019-07-06 20:23:41 -0700

    v1.2

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,45 +1,91 @@
-var cats = [
+//Main Cat array
+
+var catsArray = [
     'Abyssinian', 'Aegean', 'American Bobtail', 'American Curl', 'American Shorthair', 'American Wirehair', 'Aphrodite Giant', 'Arabian Mau', 'Asian', 'Australian Mist', 'Balinese', 'Bambino', 'Bengal', 'Birman', 'Bombay', 'Brazillian Shorthair', 'British Longhair', 'British Shorthair', 'Burmese', 'Burmilla', 'California Spangled', 'Chantilly Tiffany', 'Charteux', 'Chausie', ' Cheetoh', 'Colorpoint Shorthair', 'Cornish Rex', 'Cymric', 'Cyprus', 'Devon Rex', 'Donskoy', 'Dragon Li', 'Dwelf', 'Egyptian Mau', 'European Shorthair', 'Exotic Shorthair', 'Foldex', 'German Rex', 'Havana Brown', 'Highlander', 'Himalayan', 'Japanese Bobtail', 'Javanese', 'Karelian Bobtail', 'Khao Manee', 'Korat', 'Korean Bobtail', 'LaPerm', 'Lykoi', 'Maline Coon', 'Manx', 'Mekong Bobtail', 'Minskin', 'Munchkin', 'Napoleon', 'Ocicat', 'Ojos Azules', 'Oregon Rex', 'Oriental Bicolor', 'Persian', 'Peterbald', 'Pixie-Bob', 'Raas', 'Ragamuffin', 'Ragdoll', 'Savannah', 'Scottish Fold', 'Selkirk Rex', 'Serengeti', 'Siamese', 'Siberian', 'Singapura', 'Snowshoe', 'Sokoke', 'Somali', 'Sphynx', 'Suphalak', 'Thai Lilac', 'Tonkinese', 'Toyger', 'Turkish Angora', 'Turkish Van', 'Ukranian Levkoy', 'York Chocolate'
 ];
 
-var wins = 0;
-var loses = 0;
-var guessesLeft = 10;
-var answers = [];
-var yourGuesses = [];
-var lastGuess = false;
 
-var currentCat = cats[Math.floor(Math.random() * cats.length)]
-.toLowerCase()
-.split("")
-;
+var answers = [];
+var yourGuesses1 = [];
+var mostRecentGuess = false;
+
+var winPoints = document.getElementById("Wins");
+var guessesLeft  = document.getElementById("guessesLeft");
+var guessesLeft = 9;
+var losePoints = document.getElementById("Losses");
+var guessesSoFar = document.getElementById("yourGuesses");
+
+winPoints = 0;
+losePoints = 0;
+
+
+// Take a random cat from the array, and document this cat next to the current word in the HTML.
+
+var currentCat = catsArray[Math.floor(Math.random() * catsArray.length)].toLowerCase().split("");
+
+
+//Check random cat in the console
 
 console.log(currentCat)
+
+// Use underscores for the length of random cat (this is now working)
 
 for (let i = 0; i < currentCat.length; i++) { 
     answers[i] = '_';
 }
+document.getElementById("CurrentWord1").innerHTML = answers;
 
-document.onkeypress = function (e) {
-    console.log(e.key)
-    var userGuess = e.key
+
+//On key up, run function, "e". This function is also var "userGuess" 
+
+document.onkeyup = function (e) {
+    var userGuess = e.key;
+
+
+//If userGuess is in the currentCat array, userGuess fills out the answer array.
+
     for (let i = 0; i < currentCat.length; i++) {
         if (userGuess === currentCat[i]) {
-            answers[i] = userGuess
-            lastGuess = true
+            userGuess = answers[i]
+            mostRecentGuess = true
+            document.getElementById("yourGuesses").innerHTML = answers;
         }
-
     }
-    if (lastGuess !== true) {
-        console.log("abc");
+
+//If userguess NOT true, guessesLeft is subtracted by one. 
+
+    if (mostRecentGuess !== true) {
         guessesLeft = guessesLeft - 1;
+        document.getElementById("guessesLeft1").innerHTML = guessesLeft;
+        console.log(guessesLeft);
     }
-    lastGuess = false;
 
-    console.log(answers)
-    console.log(guessesLeft)
+    document.getElementById("yourGuesses").innerHTML = userGuess; 
+    mostRecentGuess = false;
+
+//If user runs out of guesses, the game ends. A new cat is chosen, there is an alert saying "you have been scratched", and the lives resets to 9.
+
+    if (guessesLeft === 0) {
+        alert
+    }
+
+
 }
 
-//var currentCat = document.getElementById("mainCat") {}
+
+
+
+
+//On key press, add Letter to yourGuesses
+/*document.onkeyup = function (f) {
+    userGuess = f.key;
+    document.getElementById("yourGuesses").innerHTML = userGuess; 
+
+    mostRecentGuess = false;
+
+   
+}
+
+ */
 
 
